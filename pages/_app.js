34c8969d3c569bf366ecc92393e489290d237e5f
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-04-09 08:38:58 -0400

    more work on google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css'
+import { analytics } from '@lib/firebase.js';
+
+
+const MyApp = ({ Component, pageProps }) => {
+  const routers = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent('screen_view');
+      };
+
+      routers.events.on('routeChangeComplete', logEvent);
+      logEvent(window.location.pathname);
+
+      return () => {
+        routers.events.off('routeChangeComplete', logEvent);
+      };
+    }
+  }, []);
+  return <Component {...pageProps} />;
+};
 
 export default MyApp
