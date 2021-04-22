2f5ea0134f682c35d79f0b07fa6116d86e261ef4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-04-22 17:35:43 -0400

    metatags component

---
+++ components/meta.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Meta({title, site, image, description, author}) {
+    return (
+        <Head>
+            <title>{title}</title>
+
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@fireship_dev" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
+
+            <meta name="keywords" content="henrik, van tassell, henrik tech, henrik techguy" />
+            <meta name="description" content={description} />
+            <meta name="author" content="henrik vt" />
+        </Head>
+    )
+}
