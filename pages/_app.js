a763ebf9a666e9ec559ea7d67409c51387ba0e46
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-04-25 10:06:31 -0400

    remove analytics event logging

--- pages/_app.js
+++ pages/_app.js
@@ -6,23 +6,6 @@ import { analytics } from '@lib/firebase.js';
 
 
 const MyApp = ({ Component, pageProps }) => {
-  const routers = useRouter();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      const logEvent = (url) => {
-        analytics().setCurrentScreen(url);
-        analytics().logEvent('screen_view');
-      };
-
-      routers.events.on('routeChangeComplete', logEvent);
-      logEvent(window.location.pathname);
-
-      return () => {
-        routers.events.off('routeChangeComplete', logEvent);
-      };
-    }
-  }, []);
   return <Component {...pageProps} />;
 };
 
