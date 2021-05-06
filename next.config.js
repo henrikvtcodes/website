0db07ce5af5b04fc92d0b63e4cdf1e8323171eaa
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-06 10:01:10 -0400

    set up raw loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-          module.exports = {
-            target: 'serverless',
-          }
+module.exports = withCSS({
+  cssModules: true,
+  webpack: function(config) {
+      config.module.rules.push({
+          test: /\.md$/,
+          use: 'raw-loader'
+      });
+      return config;
+  }
+});
