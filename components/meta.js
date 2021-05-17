8c75b69e97ea901ba03896842d5e0d7d88ddc361
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-17 09:15:47 -0400

    metatags

--- components/meta.js
+++ components/meta.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Head from 'next/head'
 
 
-export default function Meta({title, site, image, description, author}) {
+export default function Meta({title, image, description}) {
     let full_title = title + " | henrik's shitty website"
     return (
         <Head>
             <title>{full_title}</title>
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@henrik_tech" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
 
             <meta name="keywords" content="henrik, van tassell, henrik tech, henrik techguy" />
             <meta name="description" content={description} />
             <meta name="author" content="henrik vt" />
+
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@henrik_tech" />
+            <meta name="twitter:title" content={full_title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
+
+            <meta property="og:title" content={full_title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
         </Head>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 
-import Header from '@components/header.jsx'
-import Footer from '@components/footer.jsx'
-import styles from '@styles/Home.module.css'
+import Header from '@components/header.jsx';
+import Footer from '@components/footer.jsx';
+import Meta from '@components/meta.js';
+import styles from '@styles/Home.module.css';
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <Head>
-        <title> henrik's shitty website </title>
-        <link rel="icon" href="/henriklogo.png" />
-        <meta name="keywords" content="henrik, van tassell, henrik tech, henrik techguy" />
-        <meta name="description" content="henrik's shitty personal website" />
-        <meta name="author" content="henrik vt" />
-      </Head>
+      <Meta title='home' description='homepage' image='/henriklogo.png' />
       
       <Header /> 
 
