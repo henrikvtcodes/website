62673bb387ce555270b4cdeb59f712ee3c7c370b
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-05-17 12:08:33 -0400

    add opengraph

--- components/meta.js
+++ components/meta.js
@@ -2,25 +2,27 @@ import React from 'react';
 import Head from 'next/head'
 
 
-export default function Meta({title, image, description}) {
+export default function Meta({title, image, description, url}) {
     let full_title = title + " | henrik's shitty website"
     return (
         <Head>
             <title>{full_title}</title>
 
-            <meta name="keywords" content="henrik, van tassell, henrik tech, henrik techguy" />
-            <meta name="description" content={description} />
-            <meta name="author" content="henrik vt" />
+            <meta name='keywords' content='henrik, van tassell, henrik tech, henrik techguy' />
+            <meta name='description' content={description} />
+            <meta name='author' content='henrik vt' />
+            <link rel='icon' href='/henriklogo.png' />
 
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@henrik_tech" />
-            <meta name="twitter:title" content={full_title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
+            <meta name='twitter:card' content='summary' />
+            <meta name='twitter:site' content='@henrik_tech' />
+            <meta name='twitter:title' content={full_title} />
+            <meta name='twitter:description' content={description} />
+            <meta name='twitter:image' content='/henriklogo.png' />
 
-            <meta property="og:title" content={full_title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
+            <meta property='og:title' content={full_title} />
+            <meta property='og:description' content={description} />
+            <meta property='og:image' content='henriklogo.png' />
+            <meta property='og:url' content={url} />
         </Head>
     )
 }
