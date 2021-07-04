5677d514b01054a2ae9f8d845e951d01ecf478fd
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-07-03 22:18:55 -0400

    fix metatags error

--- components/meta.js
+++ components/meta.js
@@ -17,11 +17,12 @@ export default function Meta({title, image, description, url}) {
             <meta name='twitter:site' content='@henrik_tech' />
             <meta name='twitter:title' content={full_title} />
             <meta name='twitter:description' content={description} />
-            <meta name='twitter:image' content='/henriklogo.png' />
+            <meta name='twitter:image' content='https://next.henriktech.com/henriklogo.png' />
 
             <meta property='og:title' content={full_title} />
+            <meta property='og:type' content='website' />
             <meta property='og:description' content={description} />
-            <meta property='og:image' content='henriklogo.png' />
+            <meta property='og:image' content='https://next.henriktech.com/henriklogo.png' />
             <meta property='og:url' content={url} />
         </Head>
     )
