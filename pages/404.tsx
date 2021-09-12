ac875e47ca202f3a44e3e0af4d73e94c6804cb7c
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-09-12 02:19:46 -0400

    404 page fills viewport

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,8 +4,9 @@ import Image from 'next/image';
 
 const Error404 = () => {
     return (
-      <main className="flex">
+      <main className="grid place-items-center bg-gray-jet pagefill text-white">
         <div>
+            {/* <h1 className="UniSansHeavy text-center"> PAGE NOT FOUND </h1> */}
           <Image src="https://c.tenor.com/MKLGBC3pWMEAAAAi/dead-inside.gif" width={496} height={206} />
         </div>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,33 +12,36 @@ module.exports = {
       gridTemplateColumns: {
         'home': '[left] 20% [left-mid] 30% [mid] 30% [right-mid] 20% [right] ',
         
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        gray: {
+          manatee: '#9CA3AF',
+          jet: '#2c2f33',
+          charleston: '#23272a'
+        },
+        orange: {
+          spanish: '#e36414',
+          dark: '#fb8b24',
+          flame: '#e25822'
+        },
+        blue: {
+          celadon: '#457b9d',
+          prussian: '#1d3557',
+          pacific: '#0eb1d2',
+          azure: '#3185fc',
+          jeans: '#00a6fb'
+        },
+        purple: {
+          amethyst: '#a663cc',
+          wisteria: '#b298dc',
+          electric: '#b14aed',
+        }
       }
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      gray: {
-        manatee: '#9CA3AF',
-        jet: '#2c2f33',
-        charleston: '#23272a'
-      },
-      orange: {
-        spanish: '#e36414',
-        dark: '#fb8b24',
-        flame: '#e25822'
-      },
-      blue: {
-        celadon: '#457b9d',
-        prussian: '#1d3557',
-        pacific: '#0eb1d2',
-        azure: '#3185fc',
-        jeans: '#00a6fb'
-      },
-      purple: {
-        amethyst: '#a663cc',
-        wisteria: '#b298dc',
-        electric: '#b14aed',
-      }
+      
     },
     fontFamily: {
       sans: ['Uni Sans Heavy', 'sans-serif'],
