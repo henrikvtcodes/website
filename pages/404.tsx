5a21f919e4aa2e7546ce502b32005fe31c09b91c
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-09-12 14:02:49 -0400

    404 page gif selection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,44 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
+
 import { CustomMeta } from '~component/meta';
 import DefaultMeta from '~component/meta';
 
 
+
+const errorgifs: object = {
+  1: {
+    link: "https://c.tenor.com/MKLGBC3pWMEAAAAi/dead-inside.gif",
+    width: 496,
+    height: 206,
+    alt: 'spinning letters that say "social life: 404 not found" ',
+  },
+  2: {
+    link: "https://c.tenor.com/FcVg5W9zZJQAAAAC/error.gif",
+    width: 320,
+    height: 240,
+    alt: "gif where cat smashes computer screen after thing doesnt load",
+  },
+  3: {
+    link: "https://c.tenor.com/g6UyWxRljhwAAAAi/missing-head-mind-loading.gif",
+    width: 498,
+    height: 455,
+    alt: "person who's head is a loading spinner"
+  },
+};
+
+
 const Error404 = () => {
+    let errorgifselect: number = Math.floor(Math.random() * 3) + 1;
     return (
       <main className="grid place-items-center bg-gray-jet pagefill text-white">
+          <DefaultMeta  />
         <div>
-            <DefaultMeta  />
             {/* <h1 className="UniSansHeavy text-center"> PAGE NOT FOUND </h1> */}
-          <Image src="https://c.tenor.com/MKLGBC3pWMEAAAAi/dead-inside.gif" width={496} height={206} />
+          <Image src={errorgifs[errorgifselect].link} 
+          width={errorgifs[errorgifselect].width} 
+          height={errorgifs[errorgifselect].height} 
+          alt={errorgifs[errorgifselect].alt} />
         </div>
       </main>
     );
