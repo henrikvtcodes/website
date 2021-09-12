33b608e1c70f20891ba50fa15bf395b553fa134f
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-09-12 09:56:36 -0400

    update tailwind, postcss, autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
