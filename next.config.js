426a6cfeb1ce78184b1759a5fe7adaf809ccbf92
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-09-13 09:23:14 -0400

    roll back sentry APM install

--- next.config.js
+++ next.config.js
@@ -1,24 +1,33 @@
-const { withSentryConfig } = require('@sentry/nextjs');
+// const { withSentryConfig } = require('@sentry/nextjs');
 
-const moduleExports = {
+// const moduleExports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['c.tenor.com'],
+//   },
+// }
+
+// const SentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+
+//   authToken: process.env.SENTRY_AUTH_TOKEN,
+
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
+
+// module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+
+
+module.exports = {
   reactStrictMode: true,
+  target: 'serverless',
   images: {
     domains: ['c.tenor.com'],
   },
 }
-
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
