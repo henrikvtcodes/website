15d52fd412609fb2294babb4b8b21cda30601acc
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-11-22 10:30:49 -0500

    umami analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,18 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <!-- Cloudflare Web Analytics --> */}
-          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' 
-          data-cf-beacon='{"token": "e76ccca08dcc4e0d8e4e3752f8dc5d12"}'>
-          </script>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "e76ccca08dcc4e0d8e4e3752f8dc5d12"}'
+          ></script>
           {/* <!-- End Cloudflare Web Analytics --> */}
+          <script
+            async
+            defer
+            data-website-id="092500af-34de-4128-93c8-a7b8fdac19d9"
+            src="https://analytics.henriktech.com/umami.js"
+          ></script>
         </Head>
         <body>
           <Main />
