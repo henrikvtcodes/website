09b698d34e0d5c0005a5630c0cd1a216dcd9e24d
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-02 10:31:21 -0500

    basic pages

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+
+import StdLayout from '../layouts/standard';
+import ThemeButton from '../components/themeChange';
+
+const Home = () => {
+  return (
+    <StdLayout >
+      
+    </StdLayout>
+  )
+}
+
+export default Home
---
+++ pages/projects.tsx
@@ -0,0 +1,12 @@
+import StdLayout from '../layouts/standard';
+import ThemeButton from '../components/themeChange';
+
+const Home = () => {
+  return (
+    <StdLayout >
+      
+    </StdLayout>
+  )
+}
+
+export default Home
