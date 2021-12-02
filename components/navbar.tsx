d45d9b114de401bb7886b1efdea418a897dd4a9a
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-02 10:30:51 -0500

    functional navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import cn from "classnames";
+
+
+const NavItem = ({ href, text }:any) => {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+
+  return (
+    <Link href={href} passHref>
+      <a
+        className={cn(
+          isActive
+            ? "font-semibold text-gray-800 dark:text-gray-200"
+            : "font-normal text-gray-600 dark:text-gray-400",
+          "hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all"
+        )}
+      >
+        <span className="capsize">{text}</span>
+      </a>
+    </Link>
+  );
+}
+
+const NavBar = () => {
+
+  return( 
+  <nav className="flex flex-row flex-nowrap">
+    <NavItem href="/" text="Home" />
+    <NavItem href="/about" text="About" />
+    <NavItem href="/projects" text="Projects" />
+    <NavItem href="/contact" text="Contact" />
+  </nav>);
+};
+
+export default NavBar;
