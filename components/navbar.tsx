176ae5ee376f87b4e0c63e7b5e61c265678ec122
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-12-02 11:57:19 -0500

    navbar spacing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,12 +28,13 @@ const NavItem = ({ href, text }:any) => {
 const NavBar = () => {
 
   return( 
-  <nav className="flex flex-row flex-nowrap">
+  <nav className="flex flex-row flex-nowrap gap-4 py-2">
     <NavItem href="/" text="Home" />
     <NavItem href="/about" text="About" />
     <NavItem href="/projects" text="Projects" />
     <NavItem href="/contact" text="Contact" />
-  </nav>);
+  </nav>
+  );
 };
 
 export default NavBar;
