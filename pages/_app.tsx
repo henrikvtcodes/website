28e8b40ec80d4722d06b0cb4619ff71e9c1970a3
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-01-12 20:19:01 -0500

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
+
 import theme from "../components/themeChange";
 
 function App({ Component, pageProps }: AppProps) {
@@ -9,9 +11,15 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider defaultTheme="system">
       <ChakraProvider>
         <Component {...pageProps} />
+        <script
+          async
+          defer
+          data-website-id="5be5a980-82fd-40f0-8f84-e5bed361223b"
+          src="https://analytics.henriktech.com/umami.js"
+        ></script>
       </ChakraProvider>
     </ThemeProvider>
-  ); 
+  );
 }
 
-export default App
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <script
-            async
-            defer
-            data-website-id="5be5a980-82fd-40f0-8f84-e5bed361223b"
-            src="https://analytics.henriktech.com/umami.js"
-          ></script>
-        </Head>
+        <Head></Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
