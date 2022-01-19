8df18b23f19d9927e58d0cf76001c809a8c175b2
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-01-18 23:50:40 -0500

    script taggy thing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,25 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { ChakraProvider } from "@chakra-ui/react";
 import Script from "next/script";
+import { Provider } from "urql";
+import Head from "next/head";
 
 import theme from "../components/themeChange";
+import { hashnode } from "utils/hashnode";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider defaultTheme="system">
-      <ChakraProvider>
-        <Component {...pageProps} />
-        <Script
-          async
-          defer
-          data-website-id="5be5a980-82fd-40f0-8f84-e5bed361223b"
-          src="https://analytics.henriktech.com/umami.js"
-        />
-      </ChakraProvider>
-    </ThemeProvider>
+    // <Provider value={hashnode}>
+    <>
+      <Component {...pageProps} />
+      <Script
+        async
+        defer
+        data-website-id="5be5a980-82fd-40f0-8f84-e5bed361223b"
+        src="https://analytics.henriktech.com/umami.js"
+      />
+    </>
+    // </Provider>
   );
 }
 
