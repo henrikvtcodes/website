79231b4cbf4ca6aa4dc2ecf5ec82880ccbb9c91e
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-25 11:39:13 -0400

    add link to my consulting site

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -2,12 +2,24 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="w-full flex justify-center content-center my-4">
+    <nav className="w-full flex sm:flex-row flex-col justify-center content-center my-4 sm:gap-x-2 gap-y-2">
       <Link href={"https://blog.henriktech.com"} passHref>
-        <a className="no-underline" target={"_blank"}>
-          <span className="h-6 min-h-max px-6 py-2 align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
+        <a className="no-underline grow" target={"_blank"}>
+          <div className="w-full px-6 py-2 text-center align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
             Blog
-          </span>
+          </div>
+        </a>
+      </Link>
+      <Link
+        href={
+          "https://consulting.henrikvt.com?utm_campaign=links&utm_source=portfolio&utm_medium=web"
+        }
+        passHref
+      >
+        <a className="no-underline grow" target={"_blank"}>
+          <div className="w-full px-6 py-2 text-center align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
+            Consulting
+          </div>
         </a>
       </Link>
     </nav>
