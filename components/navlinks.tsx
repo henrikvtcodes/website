740a019ab30f56b426e70bdd39f89b708b050801
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-25 09:32:54 -0400

    fix links for real this time

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <nav className="w-full flex justify-center content-center my-4">
-      <Link href={"https://blog.henriktech.com"} passHref target={"_blank"}>
-        <a className="no-underline">
+      <Link href={"https://blog.henriktech.com"} passHref>
+        <a className="no-underline" target={"_blank"}>
           <span className="h-6 min-h-max px-6 py-2 align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
             Blog
           </span>
--- components/title.tsx
+++ components/title.tsx
@@ -4,12 +4,8 @@ import { usePlausible } from "next-plausible";
 export const Title = () => {
   const plausible = usePlausible();
   return (
-    <Link
-      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      passHref
-      target={"_blank"}
-    >
-      <a className="no-underline">
+    <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" passHref>
+      <a className="no-underline" target={"_blank"}>
         <h1 className="lg:text-7xl md:text-6xl text-5xl mt-2 mb-4 font-extrabold text-white hover:cursor-pointer">
           Henrik VT
         </h1>
