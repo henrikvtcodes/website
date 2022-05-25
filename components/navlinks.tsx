6b54cecbd028b729f2b78816675e05a2664786a3
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2022-05-25 08:44:03 -0400

    change link behavior

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <nav className="w-full flex justify-center content-center my-4">
-      <Link href={"https://blog.henriktech.com"} passHref>
+      <Link href={"https://blog.henriktech.com"} passHref target={"_blank"}>
         <a className="no-underline">
           <span className="h-6 min-h-max px-6 py-2 align-middle font-semibold text-sm text-white rounded bg-white bg-opacity-10 hover:bg-opacity-20">
             Blog
--- components/socials.tsx
+++ components/socials.tsx
@@ -8,12 +8,16 @@ const SocialBlock = (flexClass: any) => {
   return (
     <nav className="mt-2 mb-2 flex space-x-2 md:space-x-4">
       <span className=" hover:cursor-pointer hover:text-black">
-        <Link href="https://github.com/henrikvtcodes" passHref>
+        <Link
+          href="https://github.com/henrikvtcodes"
+          passHref
+          target={"_blank"}
+        >
           <GitHub className="sm:w-9 w-6 h-auto" />
         </Link>
       </span>
       <span className=" hover:cursor-pointer hover:text-blue-twitter ">
-        <Link href="https://twitter.com/henrik_tech" passHref>
+        <Link href="https://twitter.com/henrik_tech" passHref target={"_blank"}>
           <Twitter className="sm:w-9 w-6 h-auto" />
         </Link>
       </span>
