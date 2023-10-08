ac49806ed1fa9372a9d80b35c6558a121e2138f5
henrikvtcodes <commits@henrikvt.com>
2023-10-08 13:32:57 -0400

    add new font options & config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          "Inter Tight Variable",
+          "Inter Tight",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
+      },
       colors: {
         gray: {
           manatee: "#9CA3AF",
