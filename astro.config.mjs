a43f4872f8b3258580c90f8ef1b6e321af0aec8d
henrikvtcodes <commits@henrikvt.com>
2024-01-06 23:04:00 -0500

    format lmao

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
