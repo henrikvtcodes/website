f458f0f47e938753e908528fd8ad72d71cf3d2cf
henrikvtcodes <commits@henrikvt.com>
2024-01-10 09:14:57 -0500

    svelte woo!

--- astro.config.ts
+++ astro.config.ts
@@ -4,18 +4,15 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://henrikvt.com",
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["plausible"],
-      },
-    }),
-    react(),
-    sitemap(),
-  ],
-  prefetch: true,
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["plausible"]
+    }
+  }), react(), sitemap(), svelte()],
+  prefetch: true
 });
