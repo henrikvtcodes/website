dcc5040b6d225d516c65823beb817f86ce3d97df
Henrik VT <commits@henrikvt.com>
2025-02-01 17:48:42 -0500

    update some stuff and fix configs

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,19 @@
 // import partytown from "@astrojs/partytown";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://henrikvt.com",
   integrations: [tailwind(), react(), sitemap(), svelte()],
   prefetch: true,
+  image: {
+    service: passthroughImageService(),
+  },
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
