36a19ba0a476ae926e1bc5074eef45d5195c7548
Henrik VT <commits@henrikvt.com>
2025-02-01 16:57:53 -0500

    attempt stuff

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,13 @@
-import partytown from "@astrojs/partytown";
+// import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://henrikvt.com",
-  integrations: [tailwind(), partytown({
-    config: {
-      forward: ["plausible"]
-    }
-  }), react(), sitemap(), svelte()],
-  prefetch: true
+  integrations: [tailwind(), react(), sitemap(), svelte()],
+  prefetch: true,
 });
---
+++ src/pages/api/event/index.ts
@@ -0,0 +1,32 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const scriptData = await fetch("https://assets.onedollarstats.com/stonks.js");
+  const script = await scriptData.text();
+  const { status, headers } = scriptData;
+
+  return new Response(script, {
+    status,
+    headers,
+  });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.text();
+
+  const response = await fetch("https://collector.onedollarstats.com/events", {
+    body: body,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const responseBody = await response.text();
+  const { status, headers } = response;
+
+  return new Response(responseBody, {
+    status,
+    headers,
+  });
+};
