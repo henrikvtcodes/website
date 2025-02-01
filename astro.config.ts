9baa9a741f1a5dae2218affac5b7505987948d43
Henrik VT <commits@henrikvt.com>
2025-02-01 17:37:57 -0500

    add astro cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -5,9 +5,13 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://henrikvt.com",
   integrations: [tailwind(), react(), sitemap(), svelte()],
   prefetch: true,
+  output: "server",
+  adapter: cloudflare()
 });
